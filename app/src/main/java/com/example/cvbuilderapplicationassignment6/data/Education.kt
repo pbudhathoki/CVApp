d11c8d3446c60d6cbfbd03e31a63e6c784a34e14
pbudhathoki <pbudhathoki@miu.edu>
2022-12-06 17:06:53 -0600

    initial commit

---
+++ app/src/main/java/com/example/cvbuilderapplicationassignment6/data/Education.kt
@@ -0,0 +1,30 @@
+package com.example.cvbuilderapplicationassignment6.data
+
+data class Education(
+    val collegeName: String,
+    val program: String,
+    val logo: String
+) {
+
+    companion object {
+        fun getEducationList() : MutableList<Education> {
+            return mutableListOf(
+                Education(
+                    "Maharishi Internation University",
+                    "Master's in Computer Science",
+                    "https://maharishischool.org/wp-content/uploads/MaharishiSchoolTreeLogo-8cc541-green.jpg"
+                ),
+                Education(
+                    "Khalifa University/Masdar Institute",
+                    "Master in Computing and Information Science",
+                    "https://www.ku.ac.ae/wp-content/uploads/2021/08/cropped-logo.png"
+                ),
+                Education(
+                    "Advanced College of Engineering and Management",
+                    "Bachelor in Computer Engineering",
+                    "https://www.collegenp.com/uploads/2018/05/Advanced-College-of-Engineering-and-Management.jpg"
+                )
+            )
+        }
+    }
+}
---
+++ app/src/main/java/com/example/cvbuilderapplicationassignment6/ui/aboutMe/AboutMeFragment.kt
@@ -0,0 +1,36 @@
+package com.example.cvbuilderapplicationassignment6.ui.aboutMe
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+import com.example.cvbuilderapplicationassignment6.R
+import com.example.cvbuilderapplicationassignment6.data.Achievement
+import com.example.cvbuilderapplicationassignment6.data.Education
+
+class AboutMeFragment : Fragment(R.layout.fragment_about_me) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initEducation(view)
+        initAchievement(view)
+    }
+
+    private fun initEducation(view: View) {
+        val educationRecyclerView = view.findViewById<RecyclerView>(R.id.education_recycler_view)
+        val adapter = EducationAdapter(Education.getEducationList())
+        educationRecyclerView.adapter = adapter
+    }
+
+    private fun initAchievement(view: View) {
+        val AchievementRecycler = view.findViewById<RecyclerView>(R.id.Achievement_recycler_view)
+        val adapter = AchievementAdapter(Achievement.getAchievementList())
+        AchievementRecycler.adapter = adapter
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = AboutMeFragment()
+    }
+}
