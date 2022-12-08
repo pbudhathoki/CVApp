a035d517b9c4407d7a57a1a3c297c67f90ac180d
pbudhathoki <pbudhathoki@miu.edu>
2022-12-08 16:29:12 -0600

    feature addition commit

---
+++ app/src/main/java/com/example/cvbuilderapp/ui/home/HomeFragment.kt
@@ -0,0 +1,31 @@
+package com.example.cvbuilderapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.View
+import com.example.cvbuilderapp.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+
+class HomeFragment : Fragment(R.layout.fragment_home) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        initViews(view)
+    }
+
+    private fun initViews(view: View) {
+        val floatingAddButton = view.findViewById<FloatingActionButton>(R.id.floating_add_button)
+
+        floatingAddButton.setOnClickListener {
+            Snackbar.make(view, "Add Additional fields on home page", Snackbar.LENGTH_LONG).show()
+        }
+
+    }
+
+    companion object {
+        @JvmStatic
+        fun newInstance() = HomeFragment()
+    }
+}
